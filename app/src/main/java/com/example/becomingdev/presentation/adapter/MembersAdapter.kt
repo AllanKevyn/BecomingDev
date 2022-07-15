8f3a13a582f4d82ab968b7ed1c12fee79a30e3f6
Allan Kevyn <aksilvasouza@hotmail.com>
2022-07-14 22:42:37 -0300

    implemeted the adapter

---
+++ app/src/main/java/com/example/becomingdev/presentation/adapter/MembersAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.becomingdev.presentation.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.becomingdev.databinding.ItemHomeMembersBinding
+
+class MembersAdapter : RecyclerView.Adapter<MembersAdapter.Holder>(){
+
+    private var membersItems: MutableList<Any> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(ItemHomeMembersBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateItems(membersList: List<Any>) {
+        membersItems.addAll(membersList)
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(membersItems[position])
+    }
+
+    override fun getItemCount(): Int {
+        return membersItems.size
+    }
+
+    class Holder(
+        private val binding: ItemHomeMembersBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        lateinit var information: Any
+
+        fun bind(information: Any) {
+            this.information = information
+        }
+    }
+}
--- app/src/main/java/com/example/becomingdev/presentation/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/becomingdev/presentation/fragments/HomeFragment.kt
@@ -6,15 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.becomingdev.R
+import com.example.becomingdev.databinding.FragmentHomeBinding
+import com.example.becomingdev.presentation.adapter.MembersAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var membersAdapter: MembersAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false )
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setup()
+    }
 
+    private fun setup(){
+        setupObservers()
+        setupAdapters()
+    }
+
+    private fun setupObservers(){
+
+    }
+
+    private fun setupAdapters(){
+        membersAdapter = MembersAdapter()
+        binding.rvMembers.adapter = membersAdapter
+    }
 }
